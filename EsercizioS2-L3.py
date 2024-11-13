7eff469077f96a355d70f72a75d4b2b40ebd2542
KriSsHaito <krissitzhere@gmail.com>
2024-11-13 09:12:57 -0500

    first commit

---
+++ EsercizioS2-L3.py
@@ -0,0 +1,9 @@
+print(f"Ti consiglieró un nome per la tua band!")
+
+cittá = input("Dimmi il nome della tua cittá d'origine: ")
+
+nome = input("Dimmi il nome del tuo animale domestico: ")
+
+nome_band = cittá + " " + nome
+
+print(f"Il tuo nome potrebbe essere: {nome_band}")
